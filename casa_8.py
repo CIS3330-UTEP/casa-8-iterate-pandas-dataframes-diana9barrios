ee07b20ddd15281aa9f4a79909eb692a63166696
diana9barrios <dbarrios@miners.utep.edu>
2024-09-22 20:31:20 -0600

    casa8

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,11 @@
+import pandas as pd 
+df = pd.read_csv('big-mac-full-index.csv')
+df.head()
 
+#Method 4: Using iterrows() method
+for i,r in df.iterrows():
+    print(r['date']), r['name']
+
+
+#Method 6: Using apply() method
+print(df.apply(lambda row: row['name'], axis = 1))
